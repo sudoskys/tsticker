9fd5817dd9b1ea36f959ffaa3f9ad6a4c86fa5d9
sudoskys <coldlando@hotmail.com>
2024-08-18 20:20:36 +0800

    :sparkles: feat(README): update dependencies and important notes :arrow_up: chore(pyproject.toml): bump version and update dependencies

--- src/tsticker/cli.py
+++ src/tsticker/cli.py
@@ -422,7 +422,7 @@ async def create_sticker(
         return InputSticker(
             sticker=InputFile(BytesIO(sticker.data)),
             emoji_list=sticker.emojis,
-            format=sticker.sticker_type
+            format=sticker.sticker_type.__str__()
         )
     except Exception as e:
         console.print(f"[bold red]Failed to create sticker: {e}[/]")
